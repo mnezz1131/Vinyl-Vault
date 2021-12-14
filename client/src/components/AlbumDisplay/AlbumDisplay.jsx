6d6c92c9fd099ee672b4ae80227ca7ab48afd58d
Mike <mnezz1131@gmail.com>
2021-12-14 11:05:55 -0600

    card disply done

--- client/src/components/AlbumDisplay/AlbumDisplay.jsx
+++ client/src/components/AlbumDisplay/AlbumDisplay.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 import "./AlbumDisplay.css";
 
-const AlbumDisplay = ({album}) => {
+const AlbumDisplay = ({ album }) => {
   return (
     <>
-      <Link className="album-display" to={`/albums/${album.id}`}>
+      {/* <Link className="album-display" to={`/albums/${album.id}`}>
         <div className ="album-details">
           <div className="album-title">{`${album.title}`}</div>
  
@@ -20,7 +20,35 @@ const AlbumDisplay = ({album}) => {
           <div className="album-purchase">{`Purchase Date: ${album.purchased}`}</div>
           <div className="album-release">{`Release Date: ${album.released}`}</div>
         </div>
+      </Link> */}
+
+
+         <Link className="maincontainer" to={`/albums/${album.id}`}>
+        <div className ="back">
+          <div className="album-title">{`${album.title}`}</div>
+          <div>
+          <div className="album-artist">{album.artist}</div>
+          <div className="album-genre">{`Genre: ${album.genre}`}</div>
+          <div className="album-vendor">{`Vendor: ${album.vendor}`}</div>
+          <div className="album-condition">{`Condition: ${album.condition}`}</div>
+          <div className="album-cost">{`Cost: ${album.cost}`}</div>
+          <div className="album-purchase">{`Purchase Date: ${album.purchased}`}</div>
+            <div className="album-release">{`Release Date: ${album.released}`}</div>
+            </div>
+
+          </div>
+        <div className="front">
+          <div className="image">
+            <img className="cover_url" src={album.cover_url} alt={album.title} />
+          </div>
+          </div>
+        
+      
+       
       </Link>
+
+    
+    
     </>
   );
 };
