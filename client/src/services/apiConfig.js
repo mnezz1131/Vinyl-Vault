6199ec91389918aeb4ff53a3c194d7b3e91f2951
Mike <mnezz1131@gmail.com>
2021-12-14 13:39:58 -0600

    netlify redeploy

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === "production" ? "heroku_url" : 'http://localhost:3000';
+const baseUrl = process.env.NODE_ENV === "production" ? "https://vinyl-vault-api.herokuapp.com/" : 'http://localhost:3000';
 
 const api = axios.create({
   baseURL: baseUrl,
