d1e223fa774cd85e620ef3f570657f8e8c305eac
Mike <mnezz1131@gmail.com>
2021-12-15 07:36:54 -0600

    album display card v2

--- client/src/components/AlbumDisplay/AlbumDisplay.jsx
+++ client/src/components/AlbumDisplay/AlbumDisplay.jsx
@@ -4,51 +4,29 @@ import "./AlbumDisplay.css";
 const AlbumDisplay = ({ album }) => {
   return (
     <>
-      {/* <Link className="album-display" to={`/albums/${album.id}`}>
-        <div className ="album-details">
-          <div className="album-title">{`${album.title}`}</div>
- 
-          <div className="album-cover">
-            <img className="cover_url" src={album.cover_url} alt={album.title} />
-            </div>
-          <div className="album-artist">{album.artist}</div>
-      
-          <div className="album-genre">{`Genre: ${album.genre}`}</div>
-          <div className="album-vendor">{`Vendor: ${album.vendor}`}</div>
-          <div className="album-condition">{`Condition: ${album.condition}`}</div>
-          <div className="album-cost">{`Cost: ${album.cost}`}</div>
-          <div className="album-purchase">{`Purchase Date: ${album.purchased}`}</div>
-          <div className="album-release">{`Release Date: ${album.released}`}</div>
-        </div>
-      </Link> */}
-
-
-         <Link className="maincontainer" to={`/albums/${album.id}`}>
-        <div className ="back">
+      <Link className="maincontainer" to={`/albums/${album.id}`}>
+        <div className="back">
           <div className="album-title">{`${album.title}`}</div>
           <div>
-          <div className="album-artist">{album.artist}</div>
-          <div className="album-genre">{`Genre: ${album.genre}`}</div>
-          <div className="album-vendor">{`Vendor: ${album.vendor}`}</div>
-          <div className="album-condition">{`Condition: ${album.condition}`}</div>
-          <div className="album-cost">{`Cost: ${album.cost}`}</div>
-          <div className="album-purchase">{`Purchase Date: ${album.purchased}`}</div>
-            <div className="album-release">{`Release Date: ${album.released}`}</div>
-            </div>
-
+            <div className="album-display-info">{album.artist}</div>
+            <div className="album-display-info">{`Genre: ${album.genre}`}</div>
+            <div className="album-display-info">{`Vendor: ${album.vendor}`}</div>
+            <div className="album-display-info">{`Condition: ${album.condition}`}</div>
+            <div className="album-display-info">{`Cost: ${album.cost}`}</div>
+            <div className="album-display-info">{`Purchase Date: ${album.purchased}`}</div>
+            <div className="album-display-info">{`Release Date: ${album.released}`}</div>
           </div>
+        </div>
         <div className="front">
           <div className="image">
-            <img className="cover_url" src={album.cover_url} alt={album.title} />
+            <img
+              className="cover_url"
+              src={album.cover_url}
+              alt={album.title}
+            />
           </div>
-          </div>
-        
-      
-       
+        </div>
       </Link>
-
-    
-    
     </>
   );
 };
