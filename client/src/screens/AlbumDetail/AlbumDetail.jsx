ddcecd2e8d17b0a5a2569be9d94139466435f9e1
Mike <mnezz1131@gmail.com>
2021-12-15 08:00:45 -0600

    notes component

--- client/src/screens/AlbumDetail/AlbumDetail.jsx
+++ client/src/screens/AlbumDetail/AlbumDetail.jsx
@@ -8,10 +8,7 @@ const AlbumDetail = ({ user, handleLogout }) => {
   const [album, setAlbum] = useState(null);
   const [isLoaded, setLoaded] = useState(false);
   const [toggle, setToggle] = useState(false);
-  const [review, setReview] = useState({
-    title: "",
-    desciption: "",
-  });
+ 
 
   const { id } = useParams();
 
@@ -73,6 +70,39 @@ const AlbumDetail = ({ user, handleLogout }) => {
                   </Link>
                 </button>
               </div>
+                <div className="album-details-form">
+            <form  onSubmit={handleSubmit}>
+              <input 
+                className="note-input"
+                placeholder="Title"
+                value={review.title}
+                name="title"
+                required
+                onChange={handleChange}
+              />
+              <input
+                className="note-input"
+                placeholder="Enter a note"
+                value={review.desciption}
+                name="desciption"
+                required
+                onChange={handleChange}
+              />
+                <button className="album-detail-formsubmit 
+              "type="submit">Submit
+                </button>
+            </form>
+            <div className = "album-detail-notes-map">
+              {album.notes.length > 0
+                ? album.notes.map((note) => (
+                    <div className = "album-detail-notes">
+                      <h4 className = "album-detail-title">{note.title} </h4>
+                      <p className = "album-detail-note">{note.desciption}</p>
+                    </div>
+                  ))
+                : null}
+            </div>
+        </div> 
             </div>
             </div>
         
--- client/src/screens/Albums/Albums.jsx
+++ client/src/screens/Albums/Albums.jsx
@@ -2,13 +2,10 @@ import "./Albums.css";
 import Layout from "../../components/Layout/Layout.jsx";
 import AlbumDisplay from "../../components/AlbumDisplay/AlbumDisplay.jsx";
 
-
 const Albums = ({ user, handleLogout, albums }) => {
-
   return (
     <Layout user={user} handleLogout={handleLogout}>
       <div className="albums">
-        
         {albums.map((album, index) => (
           <AlbumDisplay album={album} key={index} />
         ))}
